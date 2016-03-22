83aefb0bed0c9b350e2906bdc0a8da24512f9d88
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-03-22 21:27:18 +0000

    Personal branch changes

--- spec/view_links_spec.rb
+++ spec/view_links_spec.rb
@@ -2,8 +2,12 @@ require './app/models/link'
 
 feature "View links" do
   scenario "see a list of links on the links page" do
-    Link.create(title: "Makers Academy", url: "http://www.makersacademy.com/" )
+    Link.create(url: "http://www.makersacademy.com/", title: "Makers Academy")
+
     visit "/links"
+
+    expect(page.status_code).to eq 200
+
     within "ul#links" do
       expect(page).to have_content("Makers Academy")
     end
