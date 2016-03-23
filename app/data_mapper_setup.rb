cb2f731f1a8e19f9a3f318be8b8efb192df1518b
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-23 13:17:09 +0000

    Testing fixed error take2

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,4 +3,4 @@ require_relative 'models/link'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
