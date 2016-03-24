839eb6ea6a989093081e3c9448c9bb59acfc2244
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-24 22:14:48 +0000

    Added bcrypt function ch18 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'bcrypt'
 
 class User
   include DataMapper::Resource
 
   property :id, Serial
   property :address, String
-  property :password, String
+  property :password_digest, Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   has n, :links, :through => Resource
 end
