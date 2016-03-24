800863ecb7e50fade58702e2545acd1e462fc0dc
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-24 16:50:15 +0000

    Added rakefile

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,4 +5,3 @@ require_relative 'models/tag'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
-DataMapper.auto_upgrade!
